6a8a2f1c153f9d78a2604578d860640aea8b7e11
Abi SA <abisa3002@gmail.com>
2017-12-02 16:37:36 +0700

    Done

---
+++ Kang Aswad/Kang Aswad/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Kang Aswad
+//
+//  Created by Abi Sayuti on 12/2/17.
+//  Copyright © 2017 Abi Sayuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var KangAswad: UIWebView!
+    let urlweb = "https://kangaswad.wordpress.com/"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let requesturl = NSURL(string: urlweb)
+        
+        
+        let request = NSURLRequest(url: requesturl! as URL)
+        KangAswad.loadRequest(request as URLRequest)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
